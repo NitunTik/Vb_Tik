31101280801b86442ad7f76ecfc0af649876fb48
Tik <nitun123@hotmail.com>
2018-11-27 15:05:52 +0700

    27/11/2561

---
+++ WorkSheetOne/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblSalay = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblMoney = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "เงินเดือน"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(28, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "รายได้ทั้งปี"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label3.Location = New System.Drawing.Point(28, 140)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 25)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "เงินคงเหลือทั้งหมด"
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Location = New System.Drawing.Point(136, 33)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(107, 20)
+        Me.txtSalary.TabIndex = 0
+        '
+        'btnTotal
+        '
+        Me.btnTotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnTotal.Location = New System.Drawing.Point(149, 181)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(81, 27)
+        Me.btnTotal.TabIndex = 1
+        Me.btnTotal.Text = "คำนวณ"
+        Me.btnTotal.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(28, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 25)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "เงินเดือน"
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.Location = New System.Drawing.Point(28, 105)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 25)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "หักภาษีที่ต้องชำระ"
+        '
+        'lblSalay
+        '
+        Me.lblSalay.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblSalay.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSalay.ForeColor = System.Drawing.Color.Blue
+        Me.lblSalay.Location = New System.Drawing.Point(136, 69)
+        Me.lblSalay.Name = "lblSalay"
+        Me.lblSalay.Size = New System.Drawing.Size(107, 23)
+        Me.lblSalay.TabIndex = 2
+        '
+        'lblTax
+        '
+        Me.lblTax.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblTax.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblTax.ForeColor = System.Drawing.Color.Blue
+        Me.lblTax.Location = New System.Drawing.Point(136, 105)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(107, 23)
+        Me.lblTax.TabIndex = 2
+        '
+        'lblMoney
+        '
+        Me.lblMoney.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblMoney.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblMoney.ForeColor = System.Drawing.Color.Blue
+        Me.lblMoney.Location = New System.Drawing.Point(136, 140)
+        Me.lblMoney.Name = "lblMoney"
+        Me.lblMoney.Size = New System.Drawing.Size(107, 23)
+        Me.lblMoney.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(286, 257)
+        Me.Controls.Add(Me.lblMoney)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblSalay)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtSalary As System.Windows.Forms.TextBox
+    Friend WithEvents btnTotal As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblSalay As System.Windows.Forms.Label
+    Friend WithEvents lblTax As System.Windows.Forms.Label
+    Friend WithEvents lblMoney As System.Windows.Forms.Label
+
+End Class
---
+++ WorkSheetOne/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub btnTotal_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTotal.Click
+        lblSalay.Text = txtSalary.Text * 12
+        lblTax.Text = lblSalay.Text * 0.05
+        lblMoney.Text = lblSalay.Text - lblTax.Text
+    End Sub
+End Class
