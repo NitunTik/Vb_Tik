22d848832047ed92cce11fb4efe6ef3eae93901d
Tik <nitun123@hotmail.com>
2018-12-04 11:25:36 +0700

    LAB2 4/12/2561

---
+++ WorkSheetOne/BISLab2.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BISLab2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtComputer = New System.Windows.Forms.TextBox()
+        Me.txtPrinter = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblSales = New System.Windows.Forms.Label()
+        Me.lblCommitment1 = New System.Windows.Forms.Label()
+        Me.lblCommitment2 = New System.Windows.Forms.Label()
+        Me.lblCommitmentSum = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtComputer
+        '
+        Me.txtComputer.Location = New System.Drawing.Point(189, 26)
+        Me.txtComputer.Name = "txtComputer"
+        Me.txtComputer.Size = New System.Drawing.Size(160, 20)
+        Me.txtComputer.TabIndex = 0
+        '
+        'txtPrinter
+        '
+        Me.txtPrinter.Location = New System.Drawing.Point(189, 82)
+        Me.txtPrinter.Name = "txtPrinter"
+        Me.txtPrinter.Size = New System.Drawing.Size(160, 20)
+        Me.txtPrinter.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Computer Acer Vertion x5"
+        '
+        'btnSum
+        '
+        Me.btnSum.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnSum.Location = New System.Drawing.Point(136, 290)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(91, 34)
+        Me.btnSum.TabIndex = 2
+        Me.btnSum.Text = "คำนวณ"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Printer HP MS29DN"
+        '
+        'lblSales
+        '
+        Me.lblSales.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblSales.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblSales.ForeColor = System.Drawing.Color.Blue
+        Me.lblSales.Location = New System.Drawing.Point(193, 126)
+        Me.lblSales.Name = "lblSales"
+        Me.lblSales.Size = New System.Drawing.Size(156, 20)
+        Me.lblSales.TabIndex = 1
+        '
+        'lblCommitment1
+        '
+        Me.lblCommitment1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblCommitment1.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblCommitment1.ForeColor = System.Drawing.Color.Blue
+        Me.lblCommitment1.Location = New System.Drawing.Point(193, 158)
+        Me.lblCommitment1.Name = "lblCommitment1"
+        Me.lblCommitment1.Size = New System.Drawing.Size(156, 20)
+        Me.lblCommitment1.TabIndex = 1
+        '
+        'lblCommitment2
+        '
+        Me.lblCommitment2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblCommitment2.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblCommitment2.ForeColor = System.Drawing.Color.Blue
+        Me.lblCommitment2.Location = New System.Drawing.Point(193, 190)
+        Me.lblCommitment2.Name = "lblCommitment2"
+        Me.lblCommitment2.Size = New System.Drawing.Size(156, 20)
+        Me.lblCommitment2.TabIndex = 1
+        '
+        'lblCommitmentSum
+        '
+        Me.lblCommitmentSum.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblCommitmentSum.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblCommitmentSum.ForeColor = System.Drawing.Color.Blue
+        Me.lblCommitmentSum.Location = New System.Drawing.Point(193, 224)
+        Me.lblCommitmentSum.Name = "lblCommitmentSum"
+        Me.lblCommitmentSum.Size = New System.Drawing.Size(156, 20)
+        Me.lblCommitmentSum.TabIndex = 1
+        '
+        'Label11
+        '
+        Me.Label11.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label11.Location = New System.Drawing.Point(12, 126)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(156, 20)
+        Me.Label11.TabIndex = 1
+        Me.Label11.Text = "ยอดขายรวม"
+        '
+        'Label12
+        '
+        Me.Label12.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label12.Location = New System.Drawing.Point(12, 158)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(156, 20)
+        Me.Label12.TabIndex = 1
+        Me.Label12.Text = "คอมมิชชั่น 1"
+        '
+        'Label13
+        '
+        Me.Label13.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label13.Location = New System.Drawing.Point(12, 190)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(156, 20)
+        Me.Label13.TabIndex = 1
+        Me.Label13.Text = "คอมมิชชั่น 2"
+        '
+        'Label14
+        '
+        Me.Label14.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label14.Location = New System.Drawing.Point(12, 224)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(156, 20)
+        Me.Label14.TabIndex = 1
+        Me.Label14.Text = "ค่าคอมมิชชั่นรวม"
+        '
+        'BISLab2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(375, 356)
+        Me.Controls.Add(Me.btnSum)
+        Me.Controls.Add(Me.lblCommitmentSum)
+        Me.Controls.Add(Me.lblCommitment2)
+        Me.Controls.Add(Me.lblCommitment1)
+        Me.Controls.Add(Me.lblSales)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPrinter)
+        Me.Controls.Add(Me.txtComputer)
+        Me.Name = "BISLab2"
+        Me.Text = "BISLab2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtComputer As System.Windows.Forms.TextBox
+    Friend WithEvents txtPrinter As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnSum As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblSales As System.Windows.Forms.Label
+    Friend WithEvents lblCommitment1 As System.Windows.Forms.Label
+    Friend WithEvents lblCommitment2 As System.Windows.Forms.Label
+    Friend WithEvents lblCommitmentSum As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+End Class
---
+++ WorkSheetOne/BISLab2.vb
@@ -0,0 +1,16 @@
+﻿Public Class BISLab2
+
+    Private Sub btnSum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSum.Click
+        Dim Computer, Printer, Sum, Com1, Com2, ComSum As Integer
+        Computer = Val(txtComputer.Text)
+        Printer = Val(txtPrinter.Text)
+        Sum = Computer + Printer
+        Com1 = Computer * 0.05
+        Com2 = Printer * 0.1
+        ComSum = Com1 + Com2
+        lblSales.Text = Sum
+        lblCommitment1.Text = Com1
+        lblCommitment2.Text = Com2
+        lblCommitmentSum.Text = ComSum
+    End Sub
+End Class
